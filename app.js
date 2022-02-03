9da55fed379ee0c94e73ecea2bb1bd9060ccb57e
Abhi-4793 <abhinavkrishnapy2@gmail.com>
2022-02-03 18:11:33 +0530

    procfileupdate1

--- app.js
+++ app.js
@@ -67,6 +67,6 @@ app.get("/posts/:postsid",function (req,res) {
    
  });
 
-app.listen(3000, function() {
+app.listen(Process.env.PORT||3000, function() {
   console.log("Server started on port 3000");
 });
